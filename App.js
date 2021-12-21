99d2532eacd2ba847a08232fe7694dc57bdc4fc1
Erik Peh <mephisterik01@gmail.com>
2021-12-21 23:21:51 +0800

    New Screenss

--- App.js
+++ App.js
@@ -1,20 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { StatusBar } from "expo-status-bar";
+import React, { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  FlatList,
+} from "react-native";
+import { createStackNavigator, HeaderTitle } from "@react-navigation/stack";
+import * as SQLite from "expo-sqlite";
+import { Entypo } from "@expo/vector-icons";
+import NotesStack from "./screens/noteStack";
+import AddScreen from "./screens/addScreen";
+
+const db = SQLite.openDatabase("notes.db");
+
+// export default function AddScreen({ navigation }) {
+//   return (
+//     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+//       <Text>This is the add screen</Text>
+//       <TouchableOpacity
+//         onPress={() => navigation.goBack()}
+//         style={{ padding: 10 }}
+//       >
+//         <Text style={{ color: "orange" }}>Dismiss</Text>
+//       </TouchableOpacity>
+//     </View>
+//   );
+// }
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Notes Stack"
+          component={NotesStack}
+          options={{ headerShown: false, presentation: "modal" }}
+        />
+        <Stack.Screen name="Add Note" component={AddScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "lightcyan",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ screens/noteStack.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import NotesScreen from "./noteScreen";
+
+const InnerStack = createStackNavigator();
+
+export default function NotesStack() {
+  return (
+    <InnerStack.Navigator>
+      <InnerStack.Screen
+        name="Notes"
+        component={NotesScreen}
+        options={{
+          headerTitleAlign: "center",
+          headerTitle: "Notes App",
+          headerTitleStyle: {
+            fontWeight: "bold",
+            fontSize: 30,
+          },
+          headerStyle: {
+            height: 120,
+            backgroundColor: "cyan",
+            borderBottomColor: "#ccc",
+            borderBottomWidth: 1,
+          },
+        }}
+      />
+    </InnerStack.Navigator>
+  );
+}
